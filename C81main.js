34aed074afa8a8f677d4d1cdabbf793938f8742a
Sanjana-kaul <89291910+Sanjana-kaul@users.noreply.github.com>
2022-01-30 12:48:08 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,30 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+color="black";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,60,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e){
+mouse_x=e.clientX-canvas.offsetLeft;
+mouse_y=e.clientY-canvas.offsetTop;
+circle(mouse_x,mouse_y);
+}
+
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mouse_x,mouse_y,60,0,2*Math.PI);
+    ctx.stroke();
+}
+
+
+function clearArea(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+}
